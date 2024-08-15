2d9d9f400fad285ecfb824c114dbf716a73c6034
中川　麻実 <mamingaaaz@gmail.com>
2024-08-16 00:55:56 +0900

    Firebaseのセットアップ

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDN8K6t2S5pxiW3t7p5o0asuF155XtlEpg",
+  authDomain: "daily-report-f2528.firebaseapp.com",
+  projectId: "daily-report-f2528",
+  storageBucket: "daily-report-f2528.appspot.com",
+  messagingSenderId: "615460154216",
+  appId: "1:615460154216:web:c40215dbee611ffd7e44dd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
