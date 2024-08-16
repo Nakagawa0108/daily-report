8e9f6a841433078a01329602e3770b6c55d1714e
中川　麻実 <mamingaaaz@gmail.com>
2024-08-16 19:16:53 +0900

    アプリケーションと連携

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+} from "firebase/00 firestore";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDN8K6t2S5pxiW3t7p5o0asuF155XtlEpg",
@@ -10,8 +17,59 @@ const firebaseConfig = {
   projectId: "daily-report-f2528",
   storageBucket: "daily-report-f2528.appspot.com",
   messagingSenderId: "615460154216",
-  appId: "1:615460154216:web:c40215dbee611ffd7e44dd",
+  appId: "1:615460154216:web:859cb0945bede36b7e44dd",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Cloud Firestoreの初期化
+const db = getFirestore(app);
+
+// Cloud Firestoneから取得したデータを表示する
+const fetchHistoryData = async () => {
+  let tags = "";
+
+  // reportsコレクションのデータを取得
+  const querySnapshot = await getDocs(collection(db, "reports"));
+
+  // データをテーブル表の形式に合わせてHTMLに挿入
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+    tags += `<tr><td>$(doc.data().date}</td><td>${doc.data().name}</td><td>${
+      doc.data().work
+    }</td><td>${doc.data().comment}</td></tr>`;
+  });
+  document.getElementById("js-history").innerHTML = tags;
+};
+
+// Cloud Firestoreから取得したデータを表示する
+if (document.getElementById("js-history")) {
+  fetchHistoryData(getDocs, collection, db);
+}
+
+// Cloud Firestoreにデータを送信する
+const submitData = async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  try {
+    const docRef = await addDoc(collection(db, "reports"), {
+      date: new Date(),
+      name: formData.get("name"),
+      work: formData.get("work"),
+      comment: formData.get("comment"),
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+// Cloud Firestoreにデータを送信する
+if (document.getElementById("js-form")) {
+  document
+    .getElementById("js-form")
+    .addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
+}
